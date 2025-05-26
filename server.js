0c11e558eac76ed3f00b9e3c828b2e5e5e8c0be8
nicbenji <172269214+nicbenji@users.noreply.github.com>
2025-05-26 20:31:26 +0200

    added security features, somehow fcc tests are already passing?

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 
 const fccTestingRoutes = require('./routes/fcctesting.js');
 const runner = require('./test-runner.js');
+const helmet = require('helmet');
 
 const app = express();
 
@@ -16,41 +17,50 @@ app.use('/assets', express.static(process.cwd() + '/assets'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(helmet({
+    frameguard: { actions: 'deny' },
+    noCache: true
+}));
+app.use((req, res, next) => {
+    res.setHeader("X-Powered-By", "PHP 7.4.3");
+    next();
+});
+
 //For FCC testing purposes and enables user to connect from outside the hosting platform
-app.use(cors({origin: '*'})); 
+app.use(cors({ origin: '*' }));
 
 // Index page (static HTML)
 app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  }); 
+    .get(function(req, res) {
+        res.sendFile(process.cwd() + '/views/index.html');
+    });
 
 //For FCC testing purposes
 fccTestingRoutes(app);
-    
+
 // 404 Not Found Middleware
 app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+    res.status(404)
+        .type('text')
+        .send('Not Found');
 });
 
 const portNum = process.env.PORT || 3000;
 
 // Set up server and tests
 const server = app.listen(portNum, () => {
-  console.log(`Listening on port ${portNum}`);
-  if (process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch (error) {
-        console.log('Tests are not valid:');
-        console.error(error);
-      }
-    }, 1500);
-  }
+    console.log(`Listening on port ${portNum}`);
+    if (process.env.NODE_ENV === 'test') {
+        console.log('Running Tests...');
+        setTimeout(function() {
+            try {
+                runner.run();
+            } catch (error) {
+                console.log('Tests are not valid:');
+                console.error(error);
+            }
+        }, 1500);
+    }
 });
 
 module.exports = app; // For testing
